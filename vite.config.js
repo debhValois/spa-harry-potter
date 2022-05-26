7f336af2da99e3188e9e745678fe066516c0527f
debhValois <debhvalois@gmail.com>
2022-05-26 13:21:50 -0300

    Documentation

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import viteReact from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()]
 })
+
+/* 1º PASSO DO PROJETO é criar com VITE.
+Substituto do Create React App que executa o plugin 
+São tools para views e ele já monta a estrutura da pasta do projeto
+Usar SEMPRE
+Substituir 'template vue' por 'tempate react' sem typescript na hora de criar
+Ele não baixa o node_modules. (2º passo) */
