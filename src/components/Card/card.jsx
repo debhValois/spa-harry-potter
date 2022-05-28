a85464f4fd33e695de40723efdc28647521676ee
debhValois <debhvalois@gmail.com>
2022-05-28 20:02:57 -0300

    Rename Fields CaseSensitive

--- src/components/Card/card.jsx
+++ src/components/Card/card.jsx
@@ -1,4 +1,4 @@
-import "./card.css";
+import "./Card.css";
 
 import { useState } from "react";
 import { UpdateCharacterModal } from "../UpdateCharacterModal/UpdateCharacterModal";
--- src/pages/Home/homeMocado.jsx
+++ src/pages/Home/homeMocado.jsx
@@ -1,17 +1,56 @@
+//1 - Importações do React
 import { useState, useEffect } from "react";
 
-import { Card } from "../../components/Card/Card";
+//2 - Importações do Componentes que criei
+import { Card } from "../../components/Card/card";
+/* import { data } from "../../components/Mocks/data"; */ //p mocado
 import { api } from "../../services/api";
 import { NewCharacterModal } from "../../components/NewCharacterModal/NewCharacterModal";
 
-import "./Home.css";
+//3 - Importações do CSS
+import "./home.css";
 
+/* <<< PARTE 1 - COM DATA MOCADA - ESTÁTICA >>> */
+/*  
+export function Home() {
+  //Toda vez que carrega a page mostre esses caracteres:
+  const [characters, setCharacters] = useState([]);
+  // é um array de objetos = array iterável
+
+  //Fica verificando p DATA e renderiza a page automaticamente qdo ha alterações nos cadastros sem precisar recarregar a página.
+  useEffect(() => {
+    setCharacters(data);
+  }, []);
+
+  return (
+    <>
+      <section className="cards-home">
+        {characters.map((char) => {
+          return (
+            <Card
+              key={`char_${char._id}`}
+              id={char._id}
+              name={char.name}
+              house={char.house}
+              image={char.image}
+              actor={char.actor}
+            />
+          );
+        })}
+      </section>
+    </>
+  );
+}
+ */
+
+/* <<< PARTE 2 - COM API - DINÂMICA >>> */
+ 
 export function Home() {
   const [characters, setCharacters] = useState([]);
   const [refreshCharacters, setRefreshCharacters] = useState(0);
   const [isNewCharacterModalOpen, setIsNewCharacterModalOpen] = useState(false);
 
-  function handleOpenNewCharacterModal() {
+  function handleOpenNewCharacterModal() { //button-create
     setIsNewCharacterModalOpen(true);
   }
 
@@ -23,7 +62,7 @@ export function Home() {
     setRefreshCharacters(refreshCharacters + 1);
   }
 
-  useEffect(() => {
+  useEffect(() => { // PROMISSE: PEGAR TUDO NA API P SÓ DEPOIS EXECUTAR O CODE
     api.get("/all").then((response) => setCharacters(response.data));
   }, [refreshCharacters]);
 
@@ -51,11 +90,12 @@ export function Home() {
         })}
       </section>
 
-      <NewCharacterModal
+      <NewCharacterModal // CALL MODAL + PROPERTIES
         isOpen={isNewCharacterModalOpen}
         closeModal={handleCloseNewCharacterModal}
         onCreate={onChangeCharacter}
       />
+
     </>
   );
 }
