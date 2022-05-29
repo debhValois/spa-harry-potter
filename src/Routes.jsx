f51180010312783232838e0daf63943bffddd45a
debhValois <debhvalois@gmail.com>
2022-05-28 21:27:01 -0300

    change names routes

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -4,7 +4,6 @@ import { About } from "./pages/About/About";
 import { Nav } from "./components/Nav/Nav";
 import { Footer } from "./components/Footer/Footer";
 
-
 export function AppRoutes() {
   return (
     <Router>
