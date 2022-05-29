22ca1a92c178d02c974599a2e0e063a1b9f4bdfc
debhValois <debhvalois@gmail.com>
2022-05-28 22:01:41 -0300

    routes rename

---
+++ src/routes.jsx
@@ -0,0 +1,19 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Home } from "./pages/Home/Home";
+import { About } from "./pages/About/About";
+import { Nav } from "./components/Nav/Nav";
+import { Footer } from "./components/Footer/Footer";
+
+
+export function AppRoutes() {
+  return (
+    <Router>
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+      <Footer />
+    </Router>
+  );
+}
